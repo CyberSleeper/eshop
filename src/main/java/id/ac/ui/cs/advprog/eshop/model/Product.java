e411fa7533cdec84074b0abc73d0e88cc124fc0e
CyberSleeper <mahartha.gemilang@gmail.com>
2024-02-06 16:48:41 +0700

    Add some method for Product model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -5,7 +5,25 @@ import lombok.Setter;
 
 @Getter @Setter
 public class Product {
+  private static int lastAssignedId = 0;
   private String productId;
   private String productName;
   private int productQuantity;
+
+  private String generateNextId() {
+    lastAssignedId++;
+    return String.valueOf(lastAssignedId);
+  }
+
+  public Product() {
+    this.productId = generateNextId();
+  }
+
+  public String getProductId() {
+    return productId;
+  }
+
+  public void setId(String id) {
+    this.productId = id;
+  }
 }
