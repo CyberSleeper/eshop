035c13dd466350c1f940e876545d70600d48bdbb
CyberSleeper <mahartha.gemilang@gmail.com>
2024-02-10 23:16:17 +0700

    Add ProductController Tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/WebMockTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/WebMockTest.java
@@ -2,17 +2,22 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import org.apache.hc.core5.http.HttpStatus;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
 
+@AutoConfigureJsonTesters
 @WebMvcTest(ProductController.class)
 public class WebMockTest {
   
@@ -22,22 +27,82 @@ public class WebMockTest {
   @MockBean
   private ProductService service;
 
+  @Autowired
+  private JacksonTester<Product> jsonProduct;
+
   @Test
-  public void testCreateProductPage() throws Exception {
-    mockMvc.perform(get("/product/create"))
-      .andExpect(status().isOk())
-      .andExpect(view().name("createProduct"));
+  public void canGetHomePage() throws Exception {
+    MockHttpServletResponse response = mockMvc.perform(
+        get("/"))
+        .andReturn().getResponse();
+    assert(response.getStatus() == HttpStatus.SC_OK);
   }
   @Test
-  public void testCreateProductPost() throws Exception {
-    mockMvc.perform(post("/product/create"))
-      .andExpect(status().is3xxRedirection())
-      .andExpect(view().name("redirect:list"));
+  public void canGetCreateProductPage() throws Exception {
+    MockHttpServletResponse response = mockMvc.perform(
+        get("/product/create"))
+        .andReturn().getResponse();
+    assert(response.getStatus() == HttpStatus.SC_OK);
   }
   @Test
-  public void testProductListPage() throws Exception {
-    mockMvc.perform(get("/product/list"))
-      .andExpect(status().isOk())
-      .andExpect(view().name("productList"));
+  public void canCreateProduct() throws Exception {
+    Product product = new Product();
+    product.setProductId("1");
+    product.setProductName("Product 1");
+    product.setProductQuantity(10);
+    String json = jsonProduct.write(product).getJson();
+    MockHttpServletResponse response = mockMvc.perform(
+        post("/product/create")
+        .contentType("application/json")
+        .content(json != null ? json : ""))
+        .andReturn().getResponse();
+    assert(response.getStatus() == HttpStatus.SC_MOVED_TEMPORARILY);
+  }
+  @Test
+  public void canGetProductListPage() throws Exception {
+    MockHttpServletResponse response = mockMvc.perform(
+      get("/product/list"))
+      .andReturn().getResponse();
+    assert(response.getStatus() == HttpStatus.SC_OK);
+  }
+  @Test
+  public void canGetEditProductPage() throws Exception {
+    Product product = new Product();
+    product.setProductId("1");
+    product.setProductName("Product 1");
+    product.setProductQuantity(10);
+    
+    Mockito.when(service.findById("1")).thenReturn(product);
+
+    MockHttpServletResponse response = mockMvc.perform(
+      get("/product/edit/1"))
+      .andReturn().getResponse();
+    assert(response.getStatus() == HttpStatus.SC_OK);
+  }
+  @Test
+  public void canEditProduct() throws Exception {
+    Product existingProduct = new Product("1", "Product 1", 10);
+    Product updatedProduct = new Product("1", "Product 1", 20);
+
+    Mockito.when(service.findById("1")).thenReturn(existingProduct);
+    Mockito.when(service.edit(updatedProduct)).thenReturn(updatedProduct);
+
+    String json = jsonProduct.write(updatedProduct).getJson();
+    MockHttpServletResponse response = mockMvc.perform(
+      post("/product/edit/1")
+      .contentType("application/json")
+      .content(json != null ? json : ""))
+      .andReturn().getResponse();
+    assert(response.getStatus() == HttpStatus.SC_MOVED_TEMPORARILY);
+  }
+  @Test
+  public void canDeleteProduct() throws Exception {
+    Product product = new Product("1", "Product 1", 10);
+    Mockito.when(service.delete("1")).thenReturn(product);
+
+    MockHttpServletResponse response = mockMvc.perform(
+      post("/product/delete/1"))
+      .andReturn().getResponse();
+    assert(response.getStatus() == HttpStatus.SC_MOVED_TEMPORARILY);
   }
 }
